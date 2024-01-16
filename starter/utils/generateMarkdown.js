983afc94e2ba2d6675c514708040a2ff1a42c913
timiwesco <timiwesco@yahoo.com>
2024-01-16 22:08:37 +0000

    markdown functions updated

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,6 +1,38 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+![License](https://img.shields.io/badge/License-${data.license}-blue)
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions, please contact:
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
 
 `;
 }
