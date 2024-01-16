5aea1e4816aa720621505877701076dce55e46dd
timiwesco <timiwesco@yahoo.com>
2024-01-16 22:24:23 +0000

    arrray of questions for user added

--- starter/index.js
+++ starter/index.js
@@ -4,12 +4,61 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+const questions = [{
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
+},
+{
+    type: "input",
+    name: "description",
+    message: "Provide a description of your project:"
+},
+{
+    type: "input",
+    name: "installation",
+    message: "How do you install the project?"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "How do you use the project?"
+},
+{
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: ["MIT", "Apache_2.0", "GPL_3.0", "BSD_3_Clause", "None"]
+},
+{
+    type: "input",
+    name: "contributing",
+    message: "How can others contribute to your project?"
+},
+{
+    type: "input",
+    name: "test",
+    message: "How do you run tests for your project?"
+},
+{
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?"
+},
+{
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+},
+
+
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
+
 }
 
 // function to initialize program
